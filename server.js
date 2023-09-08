114c7fa002323623087c6696cf3c31e1811c0bd1
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-08 15:18:37 -0500

    added env variables and env require to server js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const dotenv = require('dotenv');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+dotenv.config();
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
