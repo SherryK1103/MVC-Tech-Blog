737e16f8b9fb35841b7ddbbb9cc9787b603115c9
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-08 20:03:22 -0500

    made various changes including routes and connections and db

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-const dotenv = require('dotenv'); 
-const bcrypt = require('bcrypt');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+//const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-dotenv.config(); // Call dotenv.config() to load environment variables
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const dbHost = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+//app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
