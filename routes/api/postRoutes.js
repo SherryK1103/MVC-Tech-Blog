3294cbf84cfd966b484bc7625a2bcf37e0484b69
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-24 16:50:37 -0500

    changes

---
+++ routes/api/postRoutes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+const withAuth = require('../../utils/auth');
--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,11 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
   if (!req.session.logged_in) {
-    res.redirect('/login');
-  } else {
-    next();
+    return res.status(401).redirect(redirectUrl + '?error=You must be logged in to access this page');
   }
+
+  // User is logged in, continue to the next middleware or route handler
+  next();
 };
 
 module.exports = withAuth;
