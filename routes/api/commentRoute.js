d5de90e38fa9e0694305787f1b004064f38d5be1
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-30 09:54:41 -0500

    added router get for get all comments in commentRoutes file

--- routes/api/commentRoute.js
+++ routes/api/commentRoute.js
@@ -2,3 +2,15 @@ const express = require('express');
 const router = require('express').Router();
 const { Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+// Get all comments
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then((comments) => {
+            res.json(comments);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
