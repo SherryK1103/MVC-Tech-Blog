5389eb9e49be649548244f06825d57e2acbc6c17
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-30 13:46:36 -0500

    removed get all comments section from commentRoutes and added create new comment functionality in commentRoutes

--- routes/api/commentRoute.js
+++ routes/api/commentRoute.js
@@ -3,21 +3,31 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// Get all comments
-router.get('/', (req, res) => {
-    Comment.findAll()
-        .then((comments) => {
-            res.json(comments);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
+// Create a new comment
+router.post('/', async (req, res) => {
+    try {
+        // Create the comment
+        const newComment = await Comment.create({
+            comment_content: req.body.comment_content,
+            user_id: req.body.user_id,
+            post_id: req.body.post_id,
         });
+
+        // Fetch associated user
+        const user = await User.findByPk(req.body.user_id);
+
+        // Response JSON object
+        const response = {
+            id: newComment.id,
+            comment_content: newComment.comment_content,
+            date_created: newComment.date_created,
+            name: user ? user.name : null,
+        };
+        res.status(201).json(response);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to create a comment' });
+    }
 });
 
-// Create a new comment
-router.post('/', (req, res) => {
-    Comment.create({
-        comment_content: req.body.text,
-    })
-})
+module.exports = router;
