2b7b0b8e299de920d463216e9aeecc5672610556
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-22 20:06:15 -0500

    uncommented line 8 of api/index.js and added module exports router in postRoutes

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@ const commentRoutes = require('./commentRoute');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-// router.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 
 module.exports = router;
